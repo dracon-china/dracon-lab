ad6f13dfd28152b9b5db06f9cf75b8708a66bbfc
Draco <draco.coder@gmail.com>
2023-10-24 14:50:31 +0800

    feat: 🎸 添加 node 配置

---
+++ tools/eslint/javascript/node.js
@@ -0,0 +1,5 @@
+// https://www.npmjs.com/package/eslint-config-ali
+// ESlint config for Node JavaScript project
+module.exports = {
+  extends: ['eslint-config-ali/node', 'prettier'],
+};
---
+++ tools/eslint/typescript/node.js
@@ -0,0 +1,5 @@
+// https://www.npmjs.com/package/eslint-config-ali
+// ESlint config for Node TypeScript project
+module.exports = {
+  extends: ['eslint-config-ali/typescript/node', 'prettier'],
+};
