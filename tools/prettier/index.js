137ab101870210546789cf9c8765a1bd17d2665b
Draco <draco.coder@gmail.com>
2023-10-24 15:02:06 +0800

    feat: 🎸 初始化 prettier 配置

---
+++ tools/prettier/index.js
@@ -0,0 +1,21 @@
+module.exports = {
+  arrowParens: 'always',
+  plugins: [
+    require.resolve('prettier-plugin-organize-imports'),
+    require.resolve('prettier-plugin-packagejson'),
+  ],
+  printWidth: 100,
+  proseWrap: 'never',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  overrides: [
+    {
+      files: '*.md',
+      options: {
+        proseWrap: 'preserve',
+      },
+    },
+  ],
+};
